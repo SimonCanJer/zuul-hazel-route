837196dc5945bd1ed518aa149359714f945c576a
Simon <SimonCanJer@gmail.com>
2020-02-26 10:30:02 +0200

    fixed shell grammar bag

--- install.linux.sh
+++ install.linux.sh
@@ -1,43 +1,43 @@
- mypath=$(pwd)
-sudo mkdir $1 || return $?
-cd     $1 || return $?
-       git clone https://github.com/SimonCanJer/microhazle.git
-cd microhazle || return $?
-    mvn install
-echo microhazle installed into $1
-cd $1 || return $?
+mypath= "$(pwd)"
+sudo  mkdir "$1"
+cd     "$1" || return $?
+git     clone https://github.com/SimonCanJer/microhazle.git
+cd     microhazle || return $?
+mvn    install
+echo   "microhazle installed into $1"
+cd      "$1" || return $?
 
 ECHO type we must install spring lib for microhazel and routing example
  if [ ${#2} -eq 0 ]
-then
-read facade_dir
-else
-facade_dir=$2
-fi
-mkdir $facade_dir
-cd    $facade_dir || rrturn
+  then
+    read  -r facade_dir
+  else
+    facade_dir=$2
+  fi
+mkdir "$facade_dir"
+cd     "$facade_dir" || return
 
-echo    **********************************************************************************
-echo    *****       installing spring library and routed application into %facade_dir%     *****
-echo    **********************************************************************************
-   git clone https://github.com/SimonCanJer/spring-micro-hazel.git
-cd    spring-micro-hazel || return $?
-       mvn install
-cd     $mypath || return $?
-echo    **********************************************************************************
-echo    *****       running Maven to build and install the zuul application               *****
-echo    **********************************************************************************
-        mvn install
-echo  ********************** ***********************************************************
-echo  **********     ZUUL ROUTING EXAMPLE HAS BEEN INSTALLED, LOUNCHING  ALL **********
-echo  ********************** ***********************************************************
-cd   $facade_dir ||  return
-cd spring-micro-hazel\micro-hazel-spring-examples\target || return
-exec  java -jar  micro-hazel-spring-examples-1.0.4-exec.jar  backend &
+echo   "**********************************************************************************"
+echo    "***** installing spring library and routed application into %facade_dir%    *****"
+echo    "**********************************************************************************"
+git     clone https://github.com/SimonCanJer/spring-micro-hazel.git
+cd      spring-micro-hazel || return $?
+mvn     install
+cd      "$mypath" || return $?
+echo    "**********************************************************************************"
+echo    "*****       running Maven to build and install the zuul application          *****"
+echo    "**********************************************************************************"
+mvn    install
+echo  "********************** ***********************************************************"
+echo  "**********     ZUUL ROUTING EXAMPLE HAS BEEN INSTALLED, LOUNCHING  ALL  **********"
+echo  "********************** ***********************************************************"
+cd    "$facade_dir" ||  return
+cd     spring-micro-hazel\micro-hazel-spring-examples\target || return
+exec   java -jar  micro-hazel-spring-examples-1.0.4-exec.jar  backend &
 timeout 45
 exec   java -jar  micro-hazel-spring-examples-1.0.4-exec.jar  facade &
 timeout 45
-cd $mypath || return
-cd target  || return
+cd  "$mypath" || return
+cd   target  || return
 exec java -jar zuul-lb-1.0-SNAPSHOT-exec.jar &
 
